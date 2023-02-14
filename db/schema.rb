990b7c6becc6d451e66b727d2249da7f9ec9bdb2
shiv0304 <shivansh.gupta@procol.in>
2023-02-14 12:56:05 +0530

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_13_055330) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_060348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_055330) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "test"
   end
 
   create_table "slack_ids", id: :serial, force: :cascade do |t|
